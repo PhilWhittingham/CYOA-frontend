43650e9a57f1641969e369e5541fc633b29417f4
Phil W <phillip.whittingham@gmail.com>
2020-12-05 16:44:15 +0000

    Updated the void to include a restart option

--- src/components/terminals/Void.js
+++ src/components/terminals/Void.js
@@ -5,16 +5,18 @@ import { timeout } from "../../utils";
 
 export const Void = () => {
   const commands = {
-    start: {
-      description: "Begin the adventure",
+    restart: {
+      description: "Restart the adventure",
       fn: async function () {
         await timeout(2000);
+        await axios.put(API_URL_LOCATION, { location: "Start" });
+        reloadFlag(true);
         return ``;
       },
     },
   };
 
-  const welcomeText = ["You've entered the void", "-----------------------"];
+  const welcomeText = ["You've entered the void", "-----------------------", "You're here because you've, in effect, escaped the bounds of the adventure.", ".", ".", ".", "Type \"restart\" to return to the start"];
 
   return <CYOATerminal commands={commands} startText={welcomeText} />;
 };
